d3a31af216d88adfac597cabf149a2d28ba8d623
jlkauffman <jlkauffman92@gmail.com>
2018-09-28 17:36:22 -0700

    Changed folder structure

--- expressStatic.js
+++ expressStatic.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./'));
+app.use(express.static('public'));
 
 //Serves all the request which includes /img in the url from Images folder
 app.use('/img', express.static(__dirname + '/img'));
